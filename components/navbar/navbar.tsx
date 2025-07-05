bb605c3af36e6b2e461b558481b4d06ea122961f
Levitskii <68114732+Levitskii@users.noreply.github.com>
2025-07-05 19:30:09 +0300

    Update navbar menu and contact button

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,16 +1,9 @@
-import {Button, Dropdown, Link, Navbar, Text} from '@nextui-org/react';
+import {Button, Link, Navbar, Text} from '@nextui-org/react';
 import React from 'react';
-import {icons} from './icons';
 import {AcmeLogo} from './logo';
 
 export const Nav = () => {
-   const collapseItems = [
-      'Features',
-      'Customers',
-      'Pricing',
-      'Company',
-      'Legal',
-   ];
+   const collapseItems = ['Услуги', 'Решения', 'Кейсы', 'Процесс', 'Контакты'];
    return (
       <Navbar
          isBordered
@@ -34,88 +27,12 @@ export const Nav = () => {
                   pl: '6rem',
                }}
             >
-               <Dropdown isBordered>
-                  <Navbar.Item>
-                     <Dropdown.Button
-                        auto
-                        light
-                        css={{
-                           px: 0,
-                           dflex: 'center',
-                           svg: {pe: 'none'},
-                        }}
-                        iconRight={icons.chevron}
-                        ripple={false}
-                     >
-                        Features
-                     </Dropdown.Button>
-                  </Navbar.Item>
-                  <Dropdown.Menu
-                     aria-label="ACME features"
-                     css={{
-                        '$$dropdownMenuWidth': '340px',
-                        '$$dropdownItemHeight': '70px',
-                        '& .nextui-dropdown-item': {
-                           'py': '$4',
-                           'svg': {
-                              color: '$secondary',
-                              mr: '$4',
-                           },
-                           '& .nextui-dropdown-item-content': {
-                              w: '100%',
-                              fontWeight: '$semibold',
-                           },
-                        },
-                     }}
-                  >
-                     <Dropdown.Item
-                        key="autoscaling"
-                        showFullDescription
-                        description="ACME scales apps to meet user demand, automagically, based on load."
-                        icon={icons.scale}
-                     >
-                        Autoscaling
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="usage_metrics"
-                        showFullDescription
-                        description="Real-time metrics to debug issues. Slow query added? We’ll show you exactly where."
-                        icon={icons.activity}
-                     >
-                        Usage Metrics
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="production_ready"
-                        showFullDescription
-                        description="ACME runs on ACME, join us and others serving requests at web scale."
-                        icon={icons.flash}
-                     >
-                        Production Ready
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="99_uptime"
-                        showFullDescription
-                        description="Applications stay on the grid with high availability and high uptime guarantees."
-                        icon={icons.server}
-                     >
-                        +99% Uptime
-                     </Dropdown.Item>
-                     <Dropdown.Item
-                        key="supreme_support"
-                        showFullDescription
-                        description="Overcome any challenge with a supporting team ready to respond."
-                        icon={icons.user}
-                     >
-                        +Supreme Support
-                     </Dropdown.Item>
-                  </Dropdown.Menu>
-               </Dropdown>
-               <Navbar.Link isActive href="#">
-                  Customers
-               </Navbar.Link>
-               <Navbar.Link href="#">Pricing</Navbar.Link>
-               <Navbar.Link href="#">Company</Navbar.Link>
-            </Navbar.Content>
+               {collapseItems.map((item) => (
+                  <Navbar.Link key={item} href="#">
+                     {item}
+                  </Navbar.Link>
+               ))}
+               </Navbar.Content>
          </Navbar.Brand>
 
          <Navbar.Collapse>
@@ -137,7 +54,7 @@ export const Nav = () => {
          <Navbar.Content>
             <Navbar.Item>
                <Button auto flat href="#">
-                  Связаться с нами
+                  Бесплатный аудит
                </Button>
             </Navbar.Item>
          </Navbar.Content>
