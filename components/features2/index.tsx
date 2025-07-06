344d1a3553d3e1f7278b5f2cc0cab49948693949
Levitskii <68114732+Levitskii@users.noreply.github.com>
2025-07-06 22:55:54 +0300

    Add process steps section

--- components/features2/index.tsx
+++ components/features2/index.tsx
@@ -1,114 +1,114 @@
 import {Button, Divider, Text} from '@nextui-org/react';
 import React from 'react';
-import {BoxIcon} from '../icons/BoxIcon';
-import {FeatureIcon} from '../icons/FeatureIcon';
 import {Flex} from '../styles/flex';
 
+const steps = [
+  {
+    icon: '🔍',
+    title: 'Бесплатная диагностика',
+    text: 'Анализ 1-2 процессов, оценка потенциала',
+  },
+  {
+    icon: '⚡',
+    title: 'Прототипирование',
+    text: 'Рабочий MVP за 3 дня',
+  },
+  {
+    icon: '💻',
+    title: 'Разработка',
+    text: 'Поэтапное внедрение модулей',
+  },
+  {
+    icon: '🚀',
+    title: 'Запуск',
+    text: 'Обучение сотрудников и поддержка',
+  },
+  {
+    icon: '📈',
+    title: 'Анализ эффективности',
+    text: 'Отчёт по достижению KPI',
+  },
+];
+
 export const Features2 = () => {
-   return (
-      <>
-         <Flex
-            direction={'column'}
-            css={{
-               'gap': '1rem',
-               'pt': '$20',
-               'justifyContent': 'center',
-               'alignItems': 'center',
-               'px': '$6',
+  return (
+    <>
+      <Flex
+        direction={'column'}
+        css={{
+          'gap': '$10',
+          'pt': '$20',
+          'px': '$6',
+          'alignItems': 'center',
+          '@sm': { px: '$16' },
+        }}
+      >
+        <Flex direction="column" align={'center'} css={{gap: '$2'}}>
+          <Text span css={{color: '$blue600'}}>5 шагов к результату</Text>
+          <Text h3 css={{textAlign: 'center'}}>Как мы работаем</Text>
+        </Flex>
 
-               '@sm': {
-                  gap: '5rem',
-                  flexDirection: 'row-reverse',
-                  px: '$16',
-               },
-               '@md': {
-                  justifyContent: 'space-evenly',
-               },
-            }}
-         >
-            <Flex direction="column" align={'center'}>
-               <Text span css={{color: '$blue600'}}>
-                  Awesome Feature
-               </Text>
-               <Text h3>Your title here</Text>
-               <Text
-                  span
-                  css={{
-                     maxWidth: '400px',
-                     color: '$accents8',
-                  }}
-               >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl,
-                  nec tincidunt nisl lorem eu nunc. Sed euismod, nisl ut aliquam
-                  lacinia,
-               </Text>
-
-               <Flex
-                  justify={'center'}
-                  wrap={'wrap'}
-                  css={{
-                     py: '$10',
-                  }}
-               >
-                  <Flex
-                     css={{
-                        py: '$10',
-                        gap: '$5',
-                     }}
-                  >
-                     <BoxIcon />
-                     <Flex direction={'column'}>
-                        <Text h4 weight={'medium'}>
-                           Your title here
-                        </Text>
-                        <Text
-                           span
-                           css={{
-                              maxWidth: '325px',
-                              color: '$accents8',
-                           }}
-                        >
-                           Lorem ipsum dolor sit amet, consectetur adipiscing
-                           elit. Sed condimentum, nisl ut aliquam lacinia, nisl
-                           nisl aliquet nisl,
-                        </Text>
-                     </Flex>
-                  </Flex>
-                  <Flex
-                     css={{
-                        py: '$10',
-                        gap: '$5',
-                     }}
-                  >
-                     <BoxIcon />
-                     <Flex direction={'column'}>
-                        <Text h4 weight={'medium'}>
-                           Your title here
-                        </Text>
-                        <Text
-                           span
-                           css={{
-                              maxWidth: '325px',
-                              color: '$accents8',
-                           }}
-                        >
-                           Lorem ipsum dolor sit amet, consectetur adipiscing
-                           elit. Sed condimentum, nisl ut aliquam lacinia, nisl
-                           nisl aliquet nisl,
-                        </Text>
-                     </Flex>
-                  </Flex>
-               </Flex>
+        <Flex
+          justify={'between'}
+          wrap={'wrap'}
+          css={{
+            'gap': '$12',
+            'position': 'relative',
+            'maxWidth': '900px',
+            'width': '100%',
+            '@md': {
+              '&::before': {
+                content: '',
+                position: 'absolute',
+                top: '36px',
+                left: '5%',
+                right: '5%',
+                height: '1px',
+                background: '$accents3',
+              },
+            },
+          }}
+        >
+          {steps.map(({icon, title, text}, i) => (
+            <Flex
+              key={i}
+              direction={'column'}
+              align={'center'}
+              css={{
+                'textAlign': 'center',
+                'maxWidth': '160px',
+                'gap': '$4',
+                'position': 'relative',
+              }}
+            >
+              <Flex
+                css={{
+                  'position': 'absolute',
+                  'top': 0,
+                  'left': '50%',
+                  'transform': 'translate(-50%, -50%)',
+                  'width': '26px',
+                  'height': '26px',
+                  'borderRadius': '$lg',
+                  'background': '$blue600',
+                  'color': 'White',
+                  'fontWeight': '$bold',
+                  'alignItems': 'center',
+                  'justifyContent': 'center',
+                }}
+              >
+                {i + 1}
+              </Flex>
+              <Text css={{fontSize: '1.75rem'}}>{icon}</Text>
+              <Text h4 weight={'medium'}>{title}</Text>
+              <Text span css={{color: '$accents8'}}>{text}</Text>
             </Flex>
-            <Flex align={'center'}>
-               <FeatureIcon />
-            </Flex>
-         </Flex>
+          ))}
+        </Flex>
 
-         <Divider
-            css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}}
-         />
-      </>
-   );
+        <Button css={{mt: '$8'}}>Получить бесплатный аудит</Button>
+      </Flex>
+      <Divider css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}} />
+    </>
+  );
 };
