2c633b0d2fe61df92ce0f4ddaddad9be82d2c2cf
Levitskii <68114732+Levitskii@users.noreply.github.com>
2025-07-06 18:56:27 +0300

    Update feature section with business solutions

--- components/features1/index.tsx
+++ components/features1/index.tsx
@@ -1,7 +1,6 @@
-import {Divider, Grid, Text} from '@nextui-org/react';
+import {Button, Card, Divider, Grid, Text} from '@nextui-org/react';
 import React from 'react';
-import {BoxIcon} from '../icons/BoxIcon';
-import {FeatureIcon} from '../icons/FeatureIcon';
+import {CheckIcon} from '../icons/CheckIcon';
 import {Box} from '../styles/box';
 import {Flex} from '../styles/flex';
 
@@ -10,116 +9,143 @@ export const Features1 = () => {
       <>
          <Flex
             direction={'column'}
+            align={'center'}
             css={{
                'gap': '1rem',
                'pt': '$20',
-               'justifyContent': 'center',
-               'alignItems': 'center',
                'px': '$6',
                '@sm': {
-                  justifyContent: 'space-around',
-                  px: '$32',
-                  flexDirection: 'row',
+                  px: '$16',
                },
                '@md': {
                   px: '$64',
                },
             }}
          >
-            <Flex direction="column">
-               <Text span css={{color: '$blue600'}}>
-                  Awesome Feature
-               </Text>
-               <Text h3>Your title here</Text>
-               <Text
-                  span
-                  css={{
-                     maxWidth: '400px',
-                     color: '$accents8',
-                  }}
-               >
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  condimentum, nisl ut aliquam lacinia, nisl nisl aliquet nisl,
-                  nec tincidunt nisl lorem eu nunc. Sed euismod, nisl ut aliquam
-                  lacinia,
-               </Text>
+            <Text h3 css={{textAlign: 'center'}}>
+               Специализированные решения для вашего бизнеса
+            </Text>
 
-               <Flex
-                  css={{
-                     py: '$10',
-                     gap: '$5',
-                  }}
-               >
-                  <BoxIcon />
-                  <Flex direction={'column'}>
-                     <Text h4 weight={'medium'}>
-                        Your title here
-                     </Text>
-                     <Text
-                        span
-                        css={{
-                           maxWidth: '400px',
-                           color: '$accents8',
-                        }}
-                     >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed condimentum, nisl ut aliquam lacinia, nisl nisl
-                        aliquet nisl,
-                     </Text>
-                  </Flex>
-               </Flex>
-               <Flex
-                  css={{
-                     py: '$10',
-                     gap: '$5',
-                  }}
-               >
-                  <BoxIcon />
-                  <Flex direction={'column'}>
-                     <Text h4 weight={'medium'}>
-                        Your title here
-                     </Text>
-                     <Text
-                        span
-                        css={{
-                           maxWidth: '400px',
-                           color: '$accents8',
-                        }}
-                     >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed condimentum, nisl ut aliquam lacinia, nisl nisl
-                        aliquet nisl,
-                     </Text>
-                  </Flex>
-               </Flex>
-               <Flex
-                  css={{
-                     py: '$10',
-                     gap: '$5',
-                  }}
-               >
-                  <BoxIcon />
-                  <Flex direction={'column'}>
-                     <Text h4 weight={'medium'}>
-                        Your title here
-                     </Text>
-                     <Text
-                        span
-                        css={{
-                           maxWidth: '400px',
-                           color: '$accents8',
-                        }}
-                     >
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed condimentum, nisl ut aliquam lacinia, nisl nisl
-                        aliquet nisl,
-                     </Text>
-                  </Flex>
-               </Flex>
-            </Flex>
-            <Flex align={'center'}>
-               <FeatureIcon />
-            </Flex>
+            <Grid.Container gap={2} justify="center">
+               <Grid xs={12} sm={6} md={3}>
+                  <Card>
+                     <Card.Header css={{justifyContent: 'center'}}>
+                        <Text b>E-commerce</Text>
+                     </Card.Header>
+                     <Card.Body>
+                        <Box as={'ul'}>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Автоматизация заказов
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Динамическое ценообразование
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Синхронизация остатков
+                              </Text>
+                           </Flex>
+                        </Box>
+                     </Card.Body>
+                  </Card>
+               </Grid>
+               <Grid xs={12} sm={6} md={3}>
+                  <Card>
+                     <Card.Header css={{justifyContent: 'center'}}>
+                        <Text b>Маркетплейсы</Text>
+                     </Card.Header>
+                     <Card.Body>
+                        <Box as={'ul'}>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Автобронирование слотов
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Аналитика позиций
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Интеграция склада
+                              </Text>
+                           </Flex>
+                        </Box>
+                     </Card.Body>
+                  </Card>
+               </Grid>
+               <Grid xs={12} sm={6} md={3}>
+                  <Card>
+                     <Card.Header css={{justifyContent: 'center'}}>
+                        <Text b>Производство</Text>
+                     </Card.Header>
+                     <Card.Body>
+                        <Box as={'ul'}>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Контроль оборудования
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Планирование цехов
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Прогноз поломок
+                              </Text>
+                           </Flex>
+                        </Box>
+                     </Card.Body>
+                  </Card>
+               </Grid>
+               <Grid xs={12} sm={6} md={3}>
+                  <Card>
+                     <Card.Header css={{justifyContent: 'center'}}>
+                        <Text b>Строительство</Text>
+                     </Card.Header>
+                     <Card.Body>
+                        <Box as={'ul'}>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Цифровое управление проектами
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Контроль сроков
+                              </Text>
+                           </Flex>
+                           <Flex as={'li'} css={{py: '$2', gap: '$2'}} align={'center'}>
+                              <CheckIcon />
+                              <Text span css={{color: '$accents8'}}>
+                                 Мобильная отчётность
+                              </Text>
+                           </Flex>
+                        </Box>
+                     </Card.Body>
+                  </Card>
+               </Grid>
+            </Grid.Container>
+
+            <Button css={{mt: '$10'}}>Получить аудит</Button>
          </Flex>
          <Divider
             css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}}
